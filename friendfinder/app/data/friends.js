773625ee42fc4909c99217c1494790808c6d1081
MK3880 <34534769+MK3880@users.noreply.github.com>
2018-04-28 02:28:35 -0500

    comment

---
+++ friendfinder/app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+   {
+      "name":"Lightfingers Klepto Jr.",
+      "photo":"https://i.pinimg.com/originals/be/0f/49/be0f499879a9e1a5bd97210ff8c45927.jpg",
+      "scores":[
+         "5",
+         "1",
+         "4",
+         "4",
+         "5",
+         "1",
+         "2",
+         "5",
+         "4",
+         "1"
+      ]
+   },
+   {
+      "name":"Rusty Muffle",
+      "photo":"https://www.irishexaminer.com/remote/snappa.static.pressassociation.io/assets/2015/09/08141748/1441718267-65afbac5d17b3091dd82dd9e8244df47-1038x576.jpg",
+      "scores":[
+         "4",
+         "2",
+         "5",
+         "1",
+         "3",
+         "2",
+         "2",
+         "1",
+         "3",
+         "2"
+      ]
+   },
+   {
+      "name":"Junius Brown Titsworth III",
+      "photo":"http://www.potatocutter.com/fckeditor/php/upload/62011442934195.jpg",
+      "scores":[
+         "5",
+         "2",
+         "2",
+         "2",
+         "4",
+         "1",
+         "3",
+         "2",
+         "5",
+         "5"
+      ]
+   },
+   {
+      "name":"Hamburger Phillips Jr.",
+      "photo":"https://i.pinimg.com/originals/09/7b/c2/097bc28c4092e2ce720c6a25a9cc65a8.jpg",
+      "scores":[
+         "3",
+         "3",
+         "4",
+         "2",
+         "2",
+         "1",
+         "3",
+         "2",
+         "2",
+         "3"
+      ]
+   },
+	{
+	   "name":"Crazy Harry",
+	   "photo":"https://media3.giphy.com/media/11c7UUfN4eoHF6/200w.gif",
+	   "scores":[
+	      "4",
+	      "3",
+	      "4",
+	      "1",
+	      "5",
+	      "2",
+	      "5",
+	      "3",
+	      "1",
+	      "4"
+	   ]
+	},
+	{
+	   "name":"Johnny O. Nthemonorail",
+	   "photo":"https://i.pinimg.com/originals/ce/60/75/ce6075f0a605b1fea8b516398351b324.jpg",
+	   "scores":[
+	      "4",
+	      "4",
+	      "2",
+	      "3",
+	      "2",
+	      "2",
+	      "3",
+	      "2",
+	      "4",
+	      "5"
+	   ]
+	}
+
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
